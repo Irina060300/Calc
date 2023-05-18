c5ab46f07f13381889171cb1b4713dd2b88863bc
Ваше Имя <you@example.com>
2023-05-18 11:46:13 +0300

    little fix for grpaphs

--- src/calcal/graph.cpp
+++ src/calcal/graph.cpp
@@ -15,54 +15,37 @@ void Graph::slot(char *polish, t_signes *stack, t_numbers *stk, char *new_data,
 
 void Graph::create_graph(char *polish, t_signes *stack, t_numbers *stk,
                          char *new_data, double a, double b, double h) {
-  int N = (b - a) / h + 2;
+  int N = (b - a) / h + 1;
   QVector<double> x(N), y(N);
-  double X = a;
-
-  for (int i = 0; i < N; i++) {
-    double res =
-        add_result(polish, new_data, stack, stk, X);  // Формула нашей функции
+  int i = 0;
+  for (double X = a; X <= b; X = a + h * i) {
+    double res = add_result(polish, new_data, stack, stk, X);
     x[i] = X;
     y[i] = res;
-    X += h;
+    i++;
   }
   ui->widget->clearGraphs();
-
   ui->widget->addGraph();
-
   ui->widget->graph(0)->addData(x, y);
   ui->widget->graph(0)->setLineStyle(QCPGraph::lsNone);
-
   ui->widget->graph(0)->setScatterStyle(
       QCPScatterStyle(QCPScatterStyle::ssCircle, 2));
-
   ui->widget->xAxis->setLabel("x");
   ui->widget->yAxis->setLabel("y");
-
   ui->widget->xAxis->setRange(a, b);
-
-  double maxY = -100000000000, minY = 100000000000;
-  int count_maxes = 0, count_mins = 0;
-  for (int i = 1; i < N; i++) {
-    if (qIsNaN(y[i]) == false && qIsInf(y[i]) == false) {
-      if (y[i] <= minY) {
-        minY = y[i];
-        count_mins++;
-      }
-      if (y[i] >= maxY) {
-        maxY = y[i];
-        count_mins++;
-      }
+  i = 0;
+  while (isnan(y[i]) == true || isinf(y[i]) == true) i++;
+  double minY = y[i], maxY = y[i];
+  for (double k = i + 1; k < N; k++) {
+    if (isnan(y[k]) == false && isinf(y[k]) == false) {
+      if (y[k] <= minY) minY = y[k];
+      if (y[k] >= maxY) maxY = y[k];
     }
   }
-  if ((maxY - minY) > 10000 && count_maxes > 1 && count_mins > 1) {
-    double sr = maxY - (maxY - minY) / 2;
-    minY = sr - 10000;
-    maxY = sr + 10000;
-  }
+  double sr = maxY - (maxY - minY) / 2;
   if (strstr(polish, "t") != NULL) {
-    minY = -10;
-    maxY = 10;
+    minY = sr - 30;
+    maxY = sr + 30;
   }
   ui->widget->yAxis->setRange(minY - 0.2, maxY + 0.2);
   ui->widget->replot();
